696b22b227216ad7082649805cc5d88f892f8ea0
Celal Karakoç <c.karakoc@live.nl>
2025-08-17 10:42:16 +0200

    :tada: Initial commit!

---
+++ src/main/java/nl/ckarakoc/jellycash/JellycashApplication.java
@@ -0,0 +1,13 @@
+package nl.ckarakoc.jellycash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellycashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellycashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ckarakoc/jellycash/JellycashApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ckarakoc.jellycash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellycashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
